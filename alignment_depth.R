1b28097da1e4fccd9c2fb7059da91744c00dad39
Anatoly Dryga <anatolydryga@gmail.com>
2015-02-02 17:38:00 -0500

    implementation of average depth, depth distribution and coverage.

--- alignment_depth.R
+++ alignment_depth.R
@@ -1,3 +1,55 @@
-helloWorld <- function(random_list) {
-    paste("Hello World", random_list)
+#' read genome coverage file.
+#'
+#' genome coverage file is produced by:
+#' samtools view -b bam_file | genomeCoverageBed -ibam stdin > genome_coverage.tsv
+#'
+#' @return dataframe with 5 columns
+read_genomecov <- function(genome_coverage_file) {
+    if ( ! file.exists(genome_coverage_file)) {
+        stop("Genome Coverage file is not found.")
+    }
+    cov_hist <- read.delim(genome_coverage_file, header=FALSE, comment.char="#")
+    if (ncol(cov_hist) != 5) {
+        stop("Genome Coverage should have 5 columns.")
+    }
+    colnames(cov_hist) <- c("sequence", "depth", "num_bases", "length", "fraction")
+    cov_hist
+}
+
+#' average depth for sequences of genome
+#'
+#' @return dataframe with two columns: seq name, average depth
+#' 
+average_depth <- function(genome_coverage) {
+    genome_coverage$weighted_depth <- genome_coverage$depth * genome_coverage$fraction
+    ave_depth <- aggregate(weighted_depth ~ sequence, genome_coverage, sum)
+    colnames(ave_depth) <- c("sequence", "ave_depth")
+    ave_depth
+}
+
+#' coverage for sequences of genome
+#'
+#' @return dataframe with two columns: seq name, coverage
+#' 
+sequence_coverage <- function(genome_coverage) {
+    zero_coverage <- genome_coverage[ genome_coverage$depth == 0, ]
+    coverage_df <- data.frame(
+        sequence=zero_coverage$sequence,
+        coverage=1 - zero_coverage$fraction
+    )
+    coverage_df
+}
+
+#' creates depth-frequncy distribution for sequence of interest
+#'
+depth_distribution <- function(genome_coverage, sequence_name) {
+    if ( ! sequence_name %in% genome_coverage$sequence) {
+        stop("Sequence is not found. Cannot call depth_distribution.")
+    }
+    sequence_of_interest <- genome_coverage[genome_coverage$sequence == sequence_name, ]
+    depth_distribution <- data.frame(
+        depth=sequence_of_interest$depth,
+        fraction=sequence_of_interest$fraction
+    ) 
+    depth_distribution
 }
--- test_alignment_depth.R
+++ test_alignment_depth.R
@@ -1,8 +1,66 @@
 library(testthat)
 source('alignment_depth.R')
 
-context("HelloWorldTesting")
+context("Genome Coverage and Depth")
 
-test_that("prints something", {
-    expect_true(TRUE)
+coverage_histogram <- read_genomecov("sample_genomecov.txt")
+
+test_that("can read correct file", {
+    expect_equal(ncol(coverage_histogram), 5)
+    expect_equal(nrow(coverage_histogram), 12)
+})
+
+test_that("column names are correct.", {
+    expect_equal(colnames(coverage_histogram), 
+        c("sequence", "depth", "num_bases", "length", "fraction"))
+})
+
+test_that("throw exception if file does not exist", {
+    expect_error(read_genomecov("NOT_existing_file.txt"), "Genome Coverage file is not found.")
+})
+
+ave_depth <- average_depth(coverage_histogram)    
+
+test_that("have 2 sequences and genome: 3 total. And 2 columns.", {
+    expect_equal(nrow(ave_depth), 3)
+    expect_equal(ncol(ave_depth), 2)
+})
+
+test_that("column names are correct.", {
+    expect_equal(colnames(ave_depth), c("sequence", "ave_depth"))
+})
+
+test_that("sequences names are: 2,3 and genome.", {
+    expect_equal(sort(as.character(ave_depth$sequence)), c("2", "3", "genome"))
+})
+
+test_that("average depth is correct", {
+    expect_equal(ave_depth[ave_depth$sequence %in% "2", 2], 0.4533025) # only count nonzero data in sample file
+})
+
+coverage <- sequence_coverage(coverage_histogram)    
+
+test_that("coverage for 2 sequences and genome.", {
+    expect_equal(coverage[coverage$sequence %in% "2", 2], 0.894818)
+})
+
+test_that("coverage column names are correct.", {
+    expect_equal(colnames(coverage), c("sequence", "coverage"))
+})
+
+distribution <- depth_distribution(coverage_histogram, "2") 
+
+test_that("can produce depth distribution for existing sequence.", {
+    expect_equal(nrow(distribution), 4) 
+    expect_equal(ncol(distribution), 2) 
+})
+
+test_that("depth distribution names are correct", {
+    distribution <- depth_distribution(coverage_histogram, "2") 
+    expect_equal(colnames(distribution), c("depth", "fraction")) 
+})
+
+test_that("fails if sequence name is unknown..", {
+    expect_error(depth_distribution(coverage_histogram, "NON_EXISTING_SEQUENCE"), 
+        "Sequence is not found. Cannot call depth_distribution.")
 })
