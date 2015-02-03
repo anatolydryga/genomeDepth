ca8b66f5effea8c8df58af868dd85532896319b8
Anatoly Dryga <anatolydryga@gmail.com>
2015-02-03 16:01:32 -0500

    function renamed

--- test_alignment_depth.R
+++ test_alignment_depth.R
@@ -12,7 +12,7 @@ test_that("can read correct file", {
 
 test_that("column names are correct.", {
     expect_equal(colnames(coverage_histogram), 
-        c("sequence", "depth", "num_bases", "length", "fraction"))
+        c("contig_name", "depth", "num_bases", "length", "fraction"))
 })
 
 test_that("throw exception if file does not exist", {
@@ -21,36 +21,36 @@ test_that("throw exception if file does not exist", {
 
 ave_depth <- average_depth(coverage_histogram)    
 
-test_that("have 2 sequences and genome: 3 total. And 2 columns.", {
+test_that("have 2 contigs and genome: 3 total. And 2 columns.", {
     expect_equal(nrow(ave_depth), 3)
     expect_equal(ncol(ave_depth), 2)
 })
 
 test_that("column names are correct.", {
-    expect_equal(colnames(ave_depth), c("sequence", "ave_depth"))
+    expect_equal(colnames(ave_depth), c("contig_name", "ave_depth"))
 })
 
-test_that("sequences names are: 2,3 and genome.", {
-    expect_equal(sort(as.character(ave_depth$sequence)), c("2", "3", "genome"))
+test_that("contig names are: 2,3 and genome.", {
+    expect_equal(sort(as.character(ave_depth$contig_name)), c("2", "3", "genome"))
 })
 
 test_that("average depth is correct", {
-    expect_equal(ave_depth[ave_depth$sequence %in% "2", 2], 0.4533025) # only count nonzero data in sample file
+    expect_equal(ave_depth[ave_depth$contig_name %in% "2", 2], 0.4533025) # only count nonzero data in sample file
 })
 
-coverage <- sequence_coverage(coverage_histogram)    
+coverage <- contig_coverage(coverage_histogram)    
 
-test_that("coverage for 2 sequences and genome.", {
-    expect_equal(coverage[coverage$sequence %in% "2", 2], 0.894818)
+test_that("coverage for 2 contigs and genome.", {
+    expect_equal(coverage[coverage$contig_name %in% "2", 2], 0.894818)
 })
 
 test_that("coverage column names are correct.", {
-    expect_equal(colnames(coverage), c("sequence", "coverage"))
+    expect_equal(colnames(coverage), c("contig_name", "coverage"))
 })
 
 distribution <- depth_distribution(coverage_histogram, "2") 
 
-test_that("can produce depth distribution for existing sequence.", {
+test_that("can produce depth distribution for existing contigs", {
     expect_equal(nrow(distribution), 4) 
     expect_equal(ncol(distribution), 2) 
 })
@@ -60,7 +60,7 @@ test_that("depth distribution names are correct", {
     expect_equal(colnames(distribution), c("depth", "fraction")) 
 })
 
-test_that("fails if sequence name is unknown..", {
+test_that("fails if contig name is unknown..", {
     expect_error(depth_distribution(coverage_histogram, "NON_EXISTING_SEQUENCE"), 
-        "Sequence is not found. Cannot call depth_distribution.")
+        "contig_name is not found. Cannot call depth_distribution.")
 })
