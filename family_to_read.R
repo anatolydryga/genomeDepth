c7224d70b459730aff3d65a36e6a09d20876b88b
Anatoly Dryga <anatolydryga@gmail.com>
2015-02-03 16:06:32 -0500

    read contig annotation; merge read count and family annotation for a contig; read count for family

--- family_to_read.R
+++ family_to_read.R
@@ -1,15 +1,25 @@
 #' read contig annotation file
-
-get_contig_annotation <- function(contig_annotation_file) {
-    return(NULL)
+read_contig_annotation <- function(contig_annotation_file) {
+    if ( ! file.exists(contig_annotation_file)) {
+        stop("contig annotation file is not found.")
+    }
+    contig_annotation <- read.delim(contig_annotation_file)
+    if ( ! "family" %in% colnames(contig_annotation)) {
+        stop("Contig annotation should have family column.")
+    }
+    contig_annotation$contig_name <- as.character(contig_annotation$contig_name)
+    contig_annotation
 }
 
 #' for a given dataframe with info about how many reads are mapped to contig 
 #' add viral family annotation
-#' @return dataframe with 3 columns: contig name, number of reads mapped to contig, viral family.
 #'
+#' @return dataframe with 3 columns: contig name(contig_name), 
+#' number of reads mapped to contig(read_count), viral family(family).
 add_family_annotation <- function(contig_to_read_count, contig_annotation) {
-    return(NULL)
+    contig_family <- contig_annotation[, c("contig_name", "family")]
+    contig_read_family <- merge(contig_to_read_count, contig_family, all.x=TRUE)
+    contig_read_family
 }
 
 #' for each family conts how many reads were mapped to the family. 
@@ -17,5 +27,11 @@ add_family_annotation <- function(contig_to_read_count, contig_annotation) {
 #' @return dataframe with 2 columns: family name, number of reads mapped to the family.
 #' @seealso add_family_annotation
 get_family_to_read_count <- function(contig_read_family) {
-    return(NULL)
+    with_family_annotation <- aggregate(read_count ~ family, contig_read_family, sum)
+    with_family_annotation$family <- as.character(with_family_annotation$family)
+    no_family_annotation <- sum(contig_read_family[is.na(contig_read_family$family), 2])
+    family_read <- rbind(with_family_annotation, 
+        data.frame(family="UNKNOWN", read_count=no_family_annotation))
+    family_read$family <- as.factor(family_read$family)
+    family_read
 }
--- test_family_to_read.R
+++ test_family_to_read.R
@@ -1,2 +1,68 @@
+library(testthat)
+source("family_to_read.R")
+source("contig_to_read.R")
 
 context("How many reads are mapped to the families.")
+
+contig_annotation <- read_contig_annotation("sample_contig_annotation.txt")
+
+test_that("can read contig file", {
+    expect_equal(nrow(contig_annotation), 15)    
+    expect_equal(ncol(contig_annotation), 13)    
+})
+
+test_that("one of the columns is family", {
+    expect_true("family" %in% colnames(contig_annotation))
+})
+
+test_that("contig name column is a character", {
+    expect_equal(class(contig_annotation$contig_name), "character")
+})
+
+contig_to_read_count <- read_contig_to_read_count("sample_idxstats.txt")
+contig_read_family <- add_family_annotation(contig_to_read_count, contig_annotation)
+
+test_that("can merge contig to read and contig annotation dataframes", {
+    expect_equal(nrow(contig_read_family), nrow(contig_to_read_count))  
+    expect_equal(ncol(contig_read_family), 3)  
+})
+
+test_that("all contigs from contig to read count survive", {
+    expect_equal(sort(contig_to_read_count$contig_name), sort(contig_read_family$contig_name))  
+})
+
+test_that("merged df has correct names.", {
+    expect_equal(colnames(contig_read_family), c("contig_name", "read_count", "family"))  
+})
+
+test_that("merged df has correct names.", {
+    expect_equal(colnames(contig_read_family), c("contig_name", "read_count", "family"))  
+})
+
+test_that("annotation added when possible", {
+    expect_equal(sum( ! is.na(contig_read_family$family)), 8)
+    expect_equal(as.character(contig_read_family[contig_read_family$contig_name %in% "1", 3]),
+        "Myoviridae")
+    expect_equal(as.character(contig_read_family[contig_read_family$contig_name %in% "5", 3]), 
+        "Siphoviridae")
+    expect_true(is.na(contig_read_family[contig_read_family$contig_name %in% "10", 3]))
+})
+
+family_read <- get_family_to_read_count(contig_read_family)
+
+test_that("produces dataframe with 2 columns: family and read count", {
+    expect_equal(colnames(family_read), c("family", "read_count"))
+})
+
+test_that("if no family annotation exists creates family UNKNOWN.", {
+    expect_true("UNKNOWN" %in% family_read$family)
+})
+
+test_that("family and read count values are correct", {
+    expect_equal(family_read[family_read$family %in% "Myoviridae", 2], 0)
+    expect_equal(family_read[family_read$family %in% "Podoviridae", 2], 2)
+    expect_equal(family_read[family_read$family %in% "Siphoviridae", 2], 1631)
+    expect_equal(family_read[family_read$family %in% "UNKNOWN", 2], 376)
+})
+
+
