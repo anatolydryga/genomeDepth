db6252c55a072975c6d7adba5b5292dbf15435d1
Anatoly Dryga <anatolydryga@gmail.com>
2015-02-03 15:59:36 -0500

    reading idxstats file

--- contig_to_read.R
+++ contig_to_read.R
@@ -2,8 +2,17 @@
 #' 
 #' @parameter idxstats output file
 #' file is produced by: samtools idxstats bamfile > idxstats_output 
-#' @returns df with 2 columns: contig name, number of reads mapped to the contig.
-get_contig_to_read_count <- function(idxstasts_file) {
-    return(NULL)
+#' @returns df with 2 columns: contig name(contig_name), number of reads mapped to the contig(read_count).
+read_contig_to_read_count <- function(idxstasts_file) {
+    if ( ! file.exists(idxstasts_file)) {
+        stop("idxstats file is not found.")
+    }
+    contig_read_count <- read.delim(idxstasts_file, header=FALSE)
+    if (ncol(contig_read_count) != 4) {
+        stop("File format incorrest: idxstats file shuld have 4 columns.")
+    }
+    contig_read_count <- contig_read_count[-nrow(contig_read_count), c(1, 3)]
+    colnames(contig_read_count) <- c("contig_name", "read_count")
+    contig_read_count
 }
 
