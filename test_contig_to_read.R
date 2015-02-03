28849e2e94c60e480aeaeda3a48ee067a0031757
Anatoly Dryga <anatolydryga@gmail.com>
2015-02-03 16:00:12 -0500

    reading idxstats file

--- test_contig_to_read.R
+++ test_contig_to_read.R
@@ -1,3 +1,20 @@
+library(testthat)
 source("contig_to_read.R")
 
 context("How many reads are mapped to the contigs.")
+
+contig_read_count <- read_contig_to_read_count("sample_idxstats.txt")
+
+test_that("return df with 2 columns: contig_name, read_count", {
+    expect_equal(colnames(contig_read_count), c("contig_name", "read_count"))
+})
+
+test_that("removes * row from the result", {
+    expect_true( ! "*" %in% contig_read_count$contig_name)
+})
+
+test_that("correct sample file has 27 contigs.", {
+    expect_equal(nrow(contig_read_count), 27)
+    expect_equal(contig_read_count[1, 2], 0)
+    expect_equal(contig_read_count[2, 2], 1291)
+})
