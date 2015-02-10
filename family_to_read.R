548e7759acea2b59bb3c35d21e2fcef8e736016b
Anatoly Dryga <anatolydryga@gmail.com>
2015-02-10 15:26:23 -0500

    family for contig annotation

--- family_to_read.R
+++ family_to_read.R
@@ -29,9 +29,26 @@ add_family_annotation <- function(contig_to_read_count, contig_annotation) {
 get_family_to_read_count <- function(contig_read_family) {
     with_family_annotation <- aggregate(read_count ~ family, contig_read_family, sum)
     with_family_annotation$family <- as.character(with_family_annotation$family)
-    no_family_annotation <- sum(contig_read_family[is.na(contig_read_family$family), 2])
+    no_family_annotation <- sum(contig_read_family$read_count[is.na(contig_read_family$family)])
     family_read <- rbind(with_family_annotation, 
         data.frame(family="UNKNOWN", read_count=no_family_annotation))
     family_read$family <- as.factor(family_read$family)
     family_read
+}
+
+#' for each family counts how many contigs mapped to the family.
+#'
+#' @param contig_annotation df
+#' @return dataframe with 2 columns: family name, number of contigs mapped to the family.
+get_family_to_contig_count <- function(contig_annotation) {
+    # aux read_count column added to use get_family_to_read_count f
+    fam_count <- data.frame(read_count=c(1.0), family=contig_annotation$family) 
+    get_family_to_read_count(fam_count)
+}
+
+#' aux function that adds sample name to get_family_to_contig_count df
+family_contig_count_for_sample <- function(contig_annotation, sample_name) {
+    family_count <- get_family_to_contig_count(contig_annotation)
+    family_count$sample <- sample_name
+    family_count
 }
