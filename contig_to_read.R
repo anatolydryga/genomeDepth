b7dd948c5d16d269a9e958bad47d641cb3230d38
Anatoly Dryga <anatolydryga@gmail.com>
2015-04-03 14:57:28 -0400

    reading all idxstats file

--- contig_to_read.R
+++ contig_to_read.R
@@ -1,18 +1,19 @@
-#' read samtools idxstats and return how many reads mapped to each contig.
+#' read samtools idxstats and return how many reads mapped/unmapped to each contig.
 #' 
 #' @parameter idxstats output file
 #' file is produced by: samtools idxstats bamfile > idxstats_output 
-#' @returns df with 2 columns: contig name(contig_name), number of reads mapped to the contig(read_count).
-read_contig_to_read_count <- function(idxstasts_file) {
+#' @returns df with 4 columns: contig name(contig_name), contig_length,
+#' number of reads mapped to the contig(mapped), number of reads unmapped to the contig(unmapped).
+read_idxstats <- function(idxstasts_file) {
     if ( ! file.exists(idxstasts_file)) {
         stop("idxstats file is not found.")
     }
-    contig_read_count <- read.delim(idxstasts_file, header=FALSE)
-    if (ncol(contig_read_count) != 4) {
+    idx <- read.delim(idxstasts_file, header=FALSE)
+    if (ncol(idx) != 4) {
         stop("File format incorrest: idxstats file shuld have 4 columns.")
     }
-    contig_read_count <- contig_read_count[-nrow(contig_read_count), c(1, 3)]
-    colnames(contig_read_count) <- c("contig_name", "read_count")
-    contig_read_count
+    idx <- idx[ -nrow(idx), ] # remove last row with *
+    colnames(idx) <- c("contig_name", "contig_length", "mapped", "unmapped")
+    idx
 }
 
