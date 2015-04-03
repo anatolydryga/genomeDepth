bdb9331e01dfc424b465987bd626c861f9138143
Anatoly Dryga <anatolydryga@gmail.com>
2015-04-03 15:00:17 -0400

    all columns are extracted

--- test_contig_to_read.R
+++ test_contig_to_read.R
@@ -3,10 +3,10 @@ source("contig_to_read.R")
 
 context("How many reads are mapped to the contigs.")
 
-contig_read_count <- read_contig_to_read_count("sample_idxstats.txt")
+contig_read_count <- read_idxstats("sample_idxstats.txt")
 
-test_that("return df with 2 columns: contig_name, read_count", {
-    expect_equal(colnames(contig_read_count), c("contig_name", "read_count"))
+test_that("return df with 4 columns: contig_name, read_length, mapped, unmapped", {
+    expect_equal(colnames(contig_read_count), c("contig_name", "contig_length", "mapped", "unmapped"))
 })
 
 test_that("removes * row from the result", {
@@ -15,6 +15,6 @@ test_that("removes * row from the result", {
 
 test_that("correct sample file has 27 contigs.", {
     expect_equal(nrow(contig_read_count), 27)
-    expect_equal(contig_read_count[1, 2], 0)
-    expect_equal(contig_read_count[2, 2], 1291)
+    expect_equal(contig_read_count[1, 3], 0)
+    expect_equal(contig_read_count[2, 3], 1291)
 })
